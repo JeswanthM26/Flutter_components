98a073d63ee15d0237c362096a6316085b162c9a
Chethan K <chethan.k@i-exceed.com>
2025-06-12 15:10:19 +0530

    Initial Commit

---
+++ lib/country_model.dart
@@ -0,0 +1,13 @@
+class CountryModel {
+  final String isoCode;
+  final String name;
+  final String phoneCode;
+  final String flag;
+
+  CountryModel({
+    required this.isoCode,
+    required this.name,
+    required this.phoneCode,
+    required this.flag,
+  });
+}
